de30c5a69dde5847d2db0c85b72e46a95f4adf78
Andrea Angiolillo <andrea.angiolillo@mongodb.com>
2023-03-04 21:05:24 +0000

    Initial Commit

---
+++ TemperatureConversion/TemperatureConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConversionApp.swift
+//  TemperatureConversion
+//
+//  Created by Andrea on 04/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
