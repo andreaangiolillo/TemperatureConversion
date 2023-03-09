ffcfd5cedb48bd7c40489a7b5772029bb9e5ade0
Andrea Angiolillo <andrea.angiolillo@mongodb.com>
2023-03-09 18:44:31 +0000

    Added missingcode

--- TemperatureConversion/ContentView.swift
+++ TemperatureConversion/ContentView.swift
@@ -7,15 +7,85 @@
 
 import SwiftUI
 
+
+enum Units: String  {
+    case Celsius
+    case Fahrenheit
+    case Kelvin
+}
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State private var amount = 0.0
+    @State private var unitSelected = Units.Celsius
+    @State private var unitToConvert = Units.Celsius
+    
+    
+    var units = [Units.Celsius, Units.Fahrenheit, Units.Kelvin]
+    
+    
+    var convertedAmount :Double {
+        if unitSelected == unitToConvert {
+            return amount
         }
-        .padding()
+        
+        var amountToConvert = amount
+        
+        if unitSelected == Units.Kelvin {
+            amountToConvert = amount - 273.15
+        }
+        
+        if unitSelected == Units.Fahrenheit {
+            amountToConvert = (amount - 32) / 1.8
+        }
+        
+        return convertCelsiusToOtherUnit(amount: amountToConvert)
+        
+    }
+    
+    func convertCelsiusToOtherUnit(amount:Double) -> Double{
+        if unitToConvert == Units.Fahrenheit {
+            print(amount)
+            return amount * 1.8 + 32
+        }
+        
+        if unitToConvert == Units.Kelvin {
+            return amount + 273.15
+        }
+        
+        return amount
+    }
+
+    var body: some View {
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Amount", value: $amount, format:.number)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Unit", selection: $unitSelected){
+                        ForEach(units, id: \.self){
+                            Text($0.rawValue)
+                        }
+                    }.pickerStyle(.segmented)
+                } header: {
+                    Text("Amount to convert")
+                }
+                
+                
+                Section{
+                    Picker("Unit", selection: $unitToConvert){
+                        ForEach(units, id: \.self){
+                            Text($0.rawValue)
+                        }
+                    }.pickerStyle(.segmented)
+                    
+                    Text(convertedAmount, format: .number)
+                } header: {
+                    Text("Converted Amount")
+                }
+
+            }
+        }
     }
 }
 
